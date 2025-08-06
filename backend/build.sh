badd3bff337d49c89ecd10dcefefec4e03d03712
Bouaziz Ait Driss <127974293+Bouaziz-aitd@users.noreply.github.com>
2025-08-05 23:58:46 -0400

    Add files via upload

--- backend/build.sh
+++ backend/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # build.sh - Script to install backend dependencies
 
-echo "Running build.sh: Installing Python dependencies..."
+echo "Running build.sh: Installing Python dependencies from current directory..."
 pip install -r requirements.txt
 echo "Python dependencies installed."
