8b5a76c4ad619d630bf3bc6e0bd60db7dd45eab9
Bouaziz Ait Driss <127974293+Bouaziz-aitd@users.noreply.github.com>
2025-08-06 00:20:54 -0400

    Delete backend/build.sh
    Not required anymore

--- backend/build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-# build.sh - Script to install backend dependencies
-
-echo "Running build.sh: Installing Python dependencies from current directory..."
-pip install -r requirements.txt
-echo "Python dependencies installed."
